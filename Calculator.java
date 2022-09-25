3d39d0bb89925646dc29a0893129a124c2fadca5
bessna13 <110855177+bessna13@users.noreply.github.com>
2022-09-25 14:52:56 +0300

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,29 @@
+
+import java.util.function.*;
+
+public class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+
+    public static String divide(int x, int y) {
+        String errAnswer = "Деление на ноль";
+        try {
+            String answer = "Ответ" + x/y;
+           return answer;
+        }
+        catch (ArithmeticException exception){
+           return errAnswer;
+        }
+
+    }
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<String> println = System.out::println;
+
+
+}
