843d84d08bd476c5dd155f26a3e55cc4e2cad8dd
bessna13 <110855177+bessna13@users.noreply.github.com>
2022-09-27 12:18:32 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,24 +3,14 @@ import java.util.function.*;
 
 public class Calculator {
     static Supplier<Calculator> instance = Calculator::new;
+    int min = Integer.MIN_VALUE;
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> divide = (x, y) -> y == 0 ? min : x / y;
 
-    public static String divide(int x, int y) {
-        String errAnswer = "Деление на ноль";
-        try {
-            String answer = "Ответ" + x/y;
-           return answer;
-        }
-        catch (ArithmeticException exception){
-           return errAnswer;
-        }
-
-    }
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
-
     Predicate<Integer> isPositive = x -> x > 0;
 
     Consumer<String> println = System.out::println;
